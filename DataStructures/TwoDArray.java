d1bc651e16252e46bdf75a1f9c46783942479e9f
Rajat Rai <therajatraiofficial@gmail.com>
2022-03-29 05:44:07 +0530

    Add files via upload
    To declare 2D array and print its length and width

---
+++ DataStructures/TwoDArray.java
@@ -0,0 +1,8 @@
+// To decleare 2D array and print its lenth and width
+public class TwoDArray{
+    public static void main(String[] args) {
+        int [][] arr= new int[4][2];
+        System.out.println(arr.length);
+        System.out.println(arr[0].length);
+    }
+}
